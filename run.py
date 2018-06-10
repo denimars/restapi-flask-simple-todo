9accf2a705064b8951e42fc8c71c83f97f163d67
Deni Marswandi <denimarswandi@gmail.com>
2018-06-11 04:10:34 +0800

    add jwt auth

--- run.py
+++ run.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']= 'apalah-disini-ceritanya'
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+
 
 db = SQLAlchemy(app)
 api = Api(app)
@@ -18,6 +23,11 @@ def create_tables():
 
 
 import views, models, resources
+
+@jwt.token_in_blacklist_loader
+def check_token(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
 
 api.add_resource(resources.UserRegistration, '/registration')
 api.add_resource(resources.UserLogin, '/login')
